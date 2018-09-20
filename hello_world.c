94aee6c1268ad5b6859e550c3f2c7f842d8c7e4f
RobinFuchshofer <robinfuchshofer@gmail.com>
2018-09-20 09:44:21 +0200

    First Hello World in C

--- hello_world.c
+++ hello_world.c
@@ -2,15 +2,11 @@
  *				HTBLA-Leonding / Class: <your class>
  * ---------------------------------------------------------
  * Exercise Number: 0
- * Title:			<title of the assignment>
- * Author:			<your name>
+ * Title:			Hello Robin
+ * Author:			Robin Fuchshofer
  * ----------------------------------------------------------
  * Description:
- * <some brief description, what the program does: example:
- * Calculates a pyramid of numbers, i.e., it multiplies a big
- * integer by the number 2, its result by 3, etc. and then
- * returns to the original value by subsequently dividing,
- * again starting by 2, 3, etc.>
+ * Prints out "Hello Robin"
  * ----------------------------------------------------------
  */
 #include <stdio.h>
